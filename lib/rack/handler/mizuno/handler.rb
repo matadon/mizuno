4dd287d6e143104a076a5f705a09a7c6fc0ebbc2
Don Werve <donw@tardis.local>
2010-05-31 11:14:26 +0900

    Renaming to Mizuno.

---
+++ lib/rack/handler/mizuno/handler.rb
@@ -0,0 +1,69 @@
+#
+# A Rack Handler for Jetty 7.
+#
+
+require 'java'
+require 'rack'
+
+#cometd-api-1.0.0rc0.jar
+#cometd-java-server-1.0.0rc0.jar
+
+# These are various servlet filters.
+
+# Load Jetty JARs.
+jars = File.join(File.dirname(__FILE__), '..', 'java', '*.jar')
+Dir[jars].each { |j| require j }
+
+# Load the Rack/Servlet bridge.
+require 'rack/handler/mizuno/servlet'
+
+# We don't want to mix our logs in with Solr.
+# FIXME: Implement a custom logger.
+java.lang.System.setProperty("org.eclipse.jetty.util.log.class", 
+    "org.eclipse.jetty.util.log.StdErrLog")
+
+module Rack::Handler
+    class Mizuno
+	# Include various Jetty classes so we can use the short names.
+	include_class 'org.eclipse.jetty.server.Server'
+	include_class 'org.eclipse.jetty.servlet.ServletContextHandler'
+	include_class 'org.eclipse.jetty.servlet.ServletHolder'
+	include_class 'org.eclipse.jetty.server.nio.SelectChannelConnector'
+	include_class 'org.eclipse.jetty.util.thread.QueuedThreadPool'
+
+	def self.run(app, options = {})
+	    # The Jetty server
+	    server = Server.new
+
+	    # Thread pool
+	    thread_pool = QueuedThreadPool.new
+	    thread_pool.min_threads = 5
+	    thread_pool.max_threads = 50
+	    server.set_thread_pool(thread_pool)
+
+	    # Connector
+	    connector = SelectChannelConnector.new
+	    connector.setPort(options[:Port].to_i)
+	    connector.setHost(options[:Host])
+	    server.addConnector(connector)
+
+	    # Servlet context.
+	    context = ServletContextHandler.new(nil, "/", 
+		ServletContextHandler::NO_SESSIONS)
+
+	    # The servlet itself.
+	    servlet = MizunoServlet.new
+	    servlet.rackup(app)
+	    holder = ServletHolder.new(servlet)
+	    context.addServlet(holder, "/")
+
+	    # Add the context to the server and start.
+	    server.set_handler(context)
+	    puts "Started Jetty on #{connector.getHost}:#{connector.getPort}"
+	    server.start
+	end
+    end
+end
+
+# Register ourselves with Rack when this file gets loaded.
+Rack::Handler.register 'mizuno', 'Rack::Handler::Mizuno'
