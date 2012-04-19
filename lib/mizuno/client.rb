ddd75aafc40ac8779d7399dd006ba43cbd62882b
Don Werve <don.werve@gmail.com>
2012-04-19 14:19:40 -0700

    Added async HTTP client functionality via jetty-client.

---
+++ lib/mizuno/client.rb
@@ -0,0 +1,60 @@
+require 'thread'
+require 'mizuno/client_exchange'
+
+module Mizuno
+    class Client
+        include_class 'org.eclipse.jetty.client.HttpClient'
+        include_class 'org.eclipse.jetty.util.thread.QueuedThreadPool'
+
+        @lock = Mutex.new
+
+        def Client.request(*args, &block)
+            @lock.synchronize { @root ||= new }
+            @root.request(*args, &block)
+        end
+
+        def Client.stop
+            @lock.synchronize do
+                return unless @root
+                @root.stop
+                @root = nil
+            end
+        end
+
+        def initialize(options = {})
+            defaults = { :timeout => 60 }
+            options = defaults.merge(options)
+            @client = HttpClient.new
+            @client.setConnectorType(HttpClient::CONNECTOR_SELECT_CHANNEL)
+            @client.setMaxConnectionsPerAddress(100)
+            @client.setThreadPool(QueuedThreadPool.new(50))
+            @client.setTimeout(options[:timeout] * 1000)
+            @client.start
+            @lock = Mutex.new
+            @exchanges = []
+        end
+
+        def stop(wait = true)
+            wait and @lock.synchronize do
+                @exchanges.each { |e| e.waitForDone }
+                @exchanges.clear
+            end
+            @client.stop
+        end
+
+        def clear(exchange)
+            return unless @lock.try_lock
+            @exchanges.delete(exchange)
+            @lock.unlock
+        end
+
+        def request(url, options = {}, &block)
+            exchange = ClientExchange.new(self)
+            @lock.synchronize { @exchanges << exchange }
+            exchange.setup(url, options, &block)
+            @client.send(exchange)
+            return(exchange)
+        end
+    end
+end
+
---
+++ lib/mizuno/client_response.rb
@@ -0,0 +1,32 @@
+module Mizuno
+    class ClientResponse
+        include Rack::Response::Helpers
+
+        attr_accessor :url, :status, :headers, :body, :ssl, :timeout
+        
+        def initialize(url)
+            @url = url
+            @headers = Rack::Utils::HeaderHash.new
+        end
+
+        def [](key)
+            @headers[key]
+        end
+
+        def []=(key, value)
+            @headers[key] = value
+        end
+
+        def ssl?
+            @ssl == true
+        end
+
+        def timeout?
+            (@timeout == true) or (@status == 408)
+        end
+
+        def success?
+            successful? or redirect?
+        end
+    end
+end
