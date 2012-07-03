c43c0a4b50d46aa9e6d46fdb759646b7b52543c9
Badrinath Janakiraman <bjanakir@thoughtworks.com>
2012-07-02 23:48:52 -0700

    Add the ability to bring Mizuno up without configuring log4j- as you may want to configure it externally

--- lib/mizuno/logger.rb
+++ lib/mizuno/logger.rb
@@ -20,39 +20,47 @@ module Mizuno
             return if @options
             @options = options
 
-            # Default logging threshold.
-            limit = options[:warn] ? "WARN" : "ERROR"
-            limit = "DEBUG" if ($DEBUG or options[:debug])
+            unless options[:without_logging]
+              # Default logging threshold.
+              limit = options[:warn] ? "WARN" : "ERROR"
+              limit = "DEBUG" if ($DEBUG or options[:debug])
 
-            # Base logging configuration.
-            config = <<-END
-                log4j.rootCategory = #{limit}, default
-                log4j.logger.org.eclipse.jetty.util.log = #{limit}, default
-                log4j.appender.default.Threshold = #{limit}
-                log4j.appender.default.layout = org.apache.log4j.PatternLayout
-            END
+              # Base logging configuration.
+              config = <<-END
+                  log4j.rootCategory = #{limit}, default
+                  log4j.logger.org.eclipse.jetty.util.log = #{limit}, default
+                  log4j.appender.default.Threshold = #{limit}
+                  log4j.appender.default.layout = org.apache.log4j.PatternLayout
+              END
 
-            # Should we log to the console?
-            config.concat(<<-END) unless options[:log]
-                log4j.appender.default = org.apache.log4j.ConsoleAppender
-                log4j.appender.default.layout.ConversionPattern = %m\\n
-            END
+              # Should we log to the console?
+              config.concat(<<-END) unless options[:log]
+                  log4j.appender.default = org.apache.log4j.ConsoleAppender
+                  log4j.appender.default.layout.ConversionPattern = %m\\n
+              END
 
-            # Are we logging to a file?
-            config.concat(<<-END) if options[:log]
-                log4j.appender.default = org.apache.log4j.FileAppender
-                log4j.appender.default.Append = true
-                log4j.appender.default.File = #{options[:log]}
-                log4j.appender.default.layout.ConversionPattern = %d %p %m\\n
-            END
+              # Are we logging to a file?
+              config.concat(<<-END) if options[:log]
+                  log4j.appender.default = org.apache.log4j.FileAppender
+                  log4j.appender.default.Append = true
+                  log4j.appender.default.File = #{options[:log]}
+                  log4j.appender.default.layout.ConversionPattern = %d %p %m\\n
+              END
 
-            # Set up Log4J via Properties.
-            properties = Properties.new
-            properties.load(ByteArrayInputStream.new(config.to_java_bytes))
-            PropertyConfigurator.configure(properties)
+              # Set up Log4J via Properties.
+              properties = Properties.new
+              properties.load(ByteArrayInputStream.new(config.to_java_bytes))
+              PropertyConfigurator.configure(properties)
+            end  
 
             # Create the default logger that gets used everywhere.
             @logger = new
+        end
+
+        def Logger.reset
+          Java.org.apache.log4j.LogManager.shutdown
+          @options = nil
+          @logger = nil
         end
 
         def Logger.logger
